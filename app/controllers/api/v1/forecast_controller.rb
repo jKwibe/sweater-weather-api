b093aa4cbfb9584e90b3548df9ab2ff3ae028527
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-20 09:07:48 -0600

    Update the Controller with a facade

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,18 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    latitude = '39.738453'
-    longitude = '-104.984853'
-    weather_conn = Faraday.new('https://api.openweathermap.org/data/2.5/')
-    data = weather_conn.get('onecall') do |p|
-      p.params[:appid] = ENV['WEATHER_API']
-      p.params[:lat] = latitude
-      p.params[:lon] = longitude
-      p.params[:units] = 'imperial'
-    end
-
-    res = JSON.parse(data.body, symbolize_names: true)
-    forecast = ForecastData.new(res)
-
-    render json: ForecastSerializer.new(forecast)
+    render json: ForecastSerializer.new(WeatherFacade.new(params[:location]))
   end
 end
