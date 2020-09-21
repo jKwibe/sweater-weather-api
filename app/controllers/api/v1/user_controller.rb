d0178d59aa6b59d66d8fafe2eba477bd9208fd95
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-21 06:50:10 -0600

    Add create user controller.

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::UserController < ApplicationController
+  def create
+    user = User.create!(user_params)
+    token = JsonWebToken.encode(user_id: user.id)
+    token_params = { params: { access_token: token } }
+    render json: UserSerializer.new(user, token_params), status: :created
+  rescue StandardError => e
+    render json: ErrorSerializer.new(ErrorHandler.new(e)), status: :unprocessable_entity
+  end
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
