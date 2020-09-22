f75e86d19c2357f09bf9ad523540527b540fc851
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-21 18:12:49 -0600

    Remove the failing fetches.

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -3,17 +3,6 @@ class MapService
     JSON.parse(get_map_data(location).body, symbolize_names: true)
   end
 
-  def self.get_parsed_route(lat, lon, lat_to, lon_to)
-    JSON.parse(get_route(lat, lon, lat_to, lon_to).body, symbolize_names: true)
-  end
-
-  def self.get_route(lat, lon, lat_to, lon_to)
-    conn.get('route') do |p|
-      p.params[:from] = "{latLng:{lat:#{lat},lng:#{lon}}}"
-      p.params[:to] = "{latLng:{lat:#{lat_to},lng:#{lon_to}}}"
-    end
-  end
-
   def self.get_map_data(location)
     conn.get('address') do |p|
       p.params[:location] = location
